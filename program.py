f58e221c0d6319779dc5be6a825678e10fe7b33d
bdoxhobehue <almazmazitov@gmail.com>
2016-08-16 17:13:42 +0300

    Functions

--- program.py
+++ program.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import tkinter as tk
+
+
+top = tk.Tk()
+tex = tk.Text(master=top)
+tex.pack(side=tk.RIGHT)
+bop = tk.Frame()
+bop.pack(side=tk.LEFT)
+for k in range(1,10):
+    tv = 'Say {}'.format(k)
+    b = tk.Button(bop, text=tv, command=cbc(k, tex))
+    b.pack()
+
+tk.Button(bop, text='Exit', command=top.destroy).pack()
+top.mainloop()
